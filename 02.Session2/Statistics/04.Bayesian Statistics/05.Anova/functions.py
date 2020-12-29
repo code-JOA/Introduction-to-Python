483bf93ab339df5cf60f6e2327dbdd3a25b490b7
code-JOA <joshbillion1@gmail.com>
2020-12-29 19:00:11 +0000

    Commit auto_update

---
+++ 02.Session2/Statistics/04.Bayesian Statistics/05.Anova/functions.py
@@ -0,0 +1,46 @@
+import numpy as np
+import scipy.stats as stats
+
+def welch_t(a, b):
+    
+    """ Calculate Welch's t statistic for two samples. """
+
+    numerator = a.mean() - b.mean()
+    
+    # “ddof = Delta Degrees of Freedom”: the divisor used in the calculation is N - ddof, 
+    #  where N represents the number of elements. By default ddof is zero.
+    
+    denominator = np.sqrt(a.var(ddof=1)/a.size + b.var(ddof=1)/b.size)
+    
+    return np.abs(numerator/denominator)
+
+
+def welch_df(a, b):
+    
+    """ Calculate the effective degrees of freedom for two samples. This function returns the degrees of freedom """
+    
+    s1 = a.var(ddof=1) 
+    s2 = b.var(ddof=1)
+    n1 = a.size
+    n2 = b.size
+    
+    numerator = (s1/n1 + s2/n2)**2
+    denominator = (s1/ n1)**2/(n1 - 1) + (s2/ n2)**2/(n2 - 1)
+    
+    return numerator/denominator
+
+
+def p_value_welch_ttest(a, b, two_sided=False):
+    """Calculates the p-value for Welch's t-test given two samples.
+    By default, the returned p-value is for a one-sided t-test. 
+    Set the two-sided parameter to True if you wish to perform a two-sided t-test instead.
+    """
+    t = welch_t(a, b)
+    df = welch_df(a, b)
+    
+    p = 1-stats.t.cdf(np.abs(t), df)
+    
+    if two_sided:
+        return 2*p
+    else:
+        return p
